16ccb983e8636b0d409ef6b5f34c64d89f622707
Harshal Dulera <harshaldulera82@gmail.com>
2023-09-20 08:28:32 +0530

    feat(:zap:): searchbar

--- assets/main-search.js
+++ assets/main-search.js
@@ -2,6 +2,16 @@ class MainSearch extends SearchForm {
   constructor() {
     super();
     this.allSearchInputs = document.querySelectorAll('input[type="search"]');
+    this.setupEventListeners();
+  }
+
+  setupEventListeners() {
+    let allSearchForms = [];
+    this.allSearchInputs.forEach((input) => allSearchForms.push(input.form));
+    this.input.addEventListener('focus', this.onInputFocus.bind(this));
+    if (allSearchForms.length < 2) return;
+    allSearchForms.forEach((form) => form.addEventListener('reset', this.onFormReset.bind(this)));
+    this.allSearchInputs.forEach((input) => input.addEventListener('input', this.onInput.bind(this)));
   }
 
   onFormReset(event) {
@@ -14,6 +24,13 @@ class MainSearch extends SearchForm {
   onInput(event) {
     const target = event.target;
     this.keepInSync(target.value, target);
+  }
+
+  onInputFocus() {
+    const isSmallScreen = window.innerWidth < 750;
+    if (isSmallScreen) {
+      this.scrollIntoView({ behavior: 'smooth' });
+    }
   }
 
   keepInSync(value, target) {
